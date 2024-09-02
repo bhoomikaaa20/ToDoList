6ed70a5bb6a255942262a82adb6b96431647ace1
Bhoomika <bhoomikasomavarapu15@gmail.com>
2024-09-02 18:32:10 +0530

    updated commit

--- todolist/src/Create.jsx
+++ todolist/src/Create.jsx
@@ -1,27 +1,29 @@
-import React,{useState} from 'react'
-import axios from 'axios';
-import './App.css'
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 function Create() {
+  const [task, setTask] = useState();
 
-    const[task,setTask]=useState()
-
-    
-
-    const handleadd=()=>{
-        axios.post("http://localhost:3001/add",{task:task})
-        .then(result=>location.reload())
-        .catch(error=>console.log(error))
-    }
+  const handleadd = () => {
+    axios
+      .post("https://todolist-dpu4.onrender.com/add", { task: task })
+      .then((result) => location.reload())
+      .catch((error) => console.log(error));
+  };
   return (
-
-        
-        <div className="create_form">
-       <input type="text" name="" id="" placeholder='Add a task' onChange={(e)=>setTask(e.target.value)}/>
-       <button type="button" onClick={handleadd}>ADD</button>
-       </div>
-
-    
-  )
+    <div className="create_form">
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Add a task"
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button type="button" onClick={handleadd}>
+        ADD
+      </button>
+    </div>
+  );
 }
 
-export default Create
+export default Create;
--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,56 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import { BsCircleFill, BsFillCheckCircleFill, BsFillTrashFill } from 'react-icons/bs'; // Import the icons
-import Create from './Create';
-import './App.css';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import {
+  BsCircleFill,
+  BsFillCheckCircleFill,
+  BsFillTrashFill,
+} from "react-icons/bs"; // Import the icons
+import Create from "./Create";
+import "./App.css";
+import axios from "axios";
 
 function Home() {
-    const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-    const handleedit = (id) => {
-        axios.put(`http://localhost:3001/update/${id}`)
-            .then(result => location.reload())
-            .catch(error => console.log(error));
-    }
+  const handleedit = (id) => {
+    axios
+      .put(`https://todolist-dpu4.onrender.com/update/${id}`)
+      .then((result) => location.reload())
+      .catch((error) => console.log(error));
+  };
 
-    const handledelete = (id) => {
-      axios.delete(`http://localhost:3001/delete/${id}`)
-          .then(result => location.reload())
-          .catch(error => console.log(error));
-  }
-  
+  const handledelete = (id) => {
+    axios
+      .delete(`https://todolist-dpu4.onrender.com/delete/${id}`)
+      .then((result) => location.reload())
+      .catch((error) => console.log(error));
+  };
 
-    useEffect(() => {
-        axios.get('http://localhost:3001/get')
-            .then(result => setTodos(result.data))
-            .catch(error => console.log(error));
-    }, []); // <-- Empty dependency array to run the effect once
+  useEffect(() => {
+    axios
+      .get("https://todolist-dpu4.onrender.com/get")
+      .then((result) => setTodos(result.data))
+      .catch((error) => console.log(error));
+  }, []); // <-- Empty dependency array to run the effect once
 
-    return (
-        <div className='home'>
-            <h1>TODO LIST</h1>
-            <Create />
-            <div className="todo-list">
-                {todos.length === 0
-                    ? <div><h1><center>No Records</center></h1></div>
-                    : todos.map(todo => (
-                        <div className='task' key={todo._id}>
-                            <div className='checkbox' onClick={() => handleedit(todo._id)}>
-                                {todo.done
-                                    ? <BsFillCheckCircleFill className='icon-done' />
-                                    : <BsCircleFill className='icon' />
-                                }
-                                <div className={`todo-item ${todo.done ? 'done' : ''}`}>
-                                    {todo.task}
-                                </div>
-                            </div>
-                            <span><BsFillTrashFill className='icon' onClick={()=> handledelete(todo._id)}/></span>
-                        </div>
-                    ))
-                }
+  return (
+    <div className="home">
+      <h1>TODO LIST</h1>
+      <Create />
+      <div className="todo-list">
+        {todos.length === 0 ? (
+          <div>
+            <h1>
+              <center>No Records</center>
+            </h1>
+          </div>
+        ) : (
+          todos.map((todo) => (
+            <div className="task" key={todo._id}>
+              <div className="checkbox" onClick={() => handleedit(todo._id)}>
+                {todo.done ? (
+                  <BsFillCheckCircleFill className="icon-done" />
+                ) : (
+                  <BsCircleFill className="icon" />
+                )}
+                <div className={`todo-item ${todo.done ? "done" : ""}`}>
+                  {todo.task}
+                </div>
+              </div>
+              <span>
+                <BsFillTrashFill
+                  className="icon"
+                  onClick={() => handledelete(todo._id)}
+                />
+              </span>
             </div>
-        </div>
-    );
+          ))
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
